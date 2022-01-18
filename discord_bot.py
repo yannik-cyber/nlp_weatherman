fb59b1969587da6efcddd043d8897416fa8df6be
Yannik Hubrich <yannik@gridscale.io>
2022-01-18 15:46:52 +0100

    last changes

--- discord_bot.py
+++ discord_bot.py
@@ -1,5 +1,5 @@
 import discord
-import difflib
+from emoji import emojize as emo
 
 from transformers import BertTokenizer
 from transformers import BertForSequenceClassification
@@ -140,24 +140,25 @@ def get_weather(city_name):
 
 
 def get_general_weather(location, time):
-  if location == '':
-    city_name = 'Mannheim'
-  else:
-    city_name = location
+    if location == '':
+        city_name = 'Mannheim'
+    else:
+        city_name = location
 
-  weather = get_weather(city_name)
-  try:
-    alerts = weather ['alerts']
-  except KeyError:
-    alerts = ''
+    weather = get_weather(city_name)
+    try:
+        alerts = weather ['alerts']
+    except KeyError:
+        alerts = ''
 
-  if time == '' or time == 'today':
-    data = weather['daily'][0]
+    if time == '' or time == 'today':
+        data = weather['daily'][0]
+    elif time == 'tomorrow':
+        data = weather['daily'][1]
+    else:
+        data = weather['daily'][1]
 
-  elif time == 'tomorrow':
-    data = weather['daily'][1]
-
-  weather_data = {'temp_morn' : int(float(data['feels_like']['morn']) - 273.15),
+    weather_data = {'temp_morn' : int(float(data['feels_like']['morn']) - 273.15),
                   'temp_eve' : int(float(data['feels_like']['eve']) - 273.15),
                   'temp_night' : int(float(data['feels_like']['night']) - 273.15),
                   'temp_day' : int(float(data['temp']['day']) - 273.15),
@@ -167,8 +168,7 @@ def get_general_weather(location, time):
                   'rain_prob' : int(float(data['pop']) * 100),
                   'clouds' : data['clouds'],
                   'wind' : data['wind_speed']}
-
-  return weather_data, alerts
+    return weather_data, alerts
 
 
 def get_rain(location, time):
@@ -225,6 +225,11 @@ def get_rain(location, time):
             rain = 'yes'
             rain_prob = prob
 
+    else:
+        rain = 'maybe'
+        rain_prob = 50
+
+
     return rain, rain_prob
 
 
@@ -246,6 +251,9 @@ def get_temperature(location, time):
     elif time == 'tomorrow':
         temp = weather['daily'][1]['feels_like']
 
+    else:
+        temp = weather['daily'][1]['feels_like']
+
     return temp
 
 def get_sun_hours(location, time):
@@ -264,7 +272,11 @@ def get_sun_hours(location, time):
     elif time == 'tomorrow':
         sunrise = pd.to_datetime(int(weather['daily'][1]['sunrise']), unit='s')
         sunset = pd.to_datetime(int(weather['daily'][1]['sunset']), unit='s')
+    else:
+        sunrise = pd.to_datetime(int(weather['daily'][0]['sunrise']), unit='s')
+        sunset = pd.to_datetime(int(weather['daily'][0]['sunset']), unit='s')
     return sunrise, sunset, city_name
+
 #-----------
 #discord bot
 #-----------
@@ -298,9 +310,10 @@ async def on_message(message):
 
 # intent: Bye
         if intent == 0:
-            responses = ['Bye, have a nice day.',
-                         'Bye bye',
-                         'Farewell traveler.']
+            bye_emoji = u'\U0001F44B'
+            responses = [f'Bye, have a nice day. {bye_emoji}',
+                         f'Bye bye {bye_emoji}',
+                         f'Farewell traveler {bye_emoji}']
             response = random.choice(responses)
 
 # intent cloud
@@ -308,20 +321,25 @@ async def on_message(message):
             location, time = entity_recognition(content)
 
             data, alerts = get_general_weather(location, time)
+
+            sun_emoji = u'\U00002600'
+            cloud_emoji = u'\U00002601'
+            cloud_and_sun_emoji = u'\U000026C5'
+
             if data['clouds'] < 33:
-                responses = [f'It is sunny',
-                             f'Take you sunglasses out. It is blue sky',
-                             f'The sky is blue.'
+                responses = [f'{sun_emoji} It is sunny in {location.capitalize()}',
+                             f'{sun_emoji} Take your sunglasses out. In {location.capitalize()} it is blue sky',
+                             f'{sun_emoji} The sky is blue in {location.capitalize()}.'
                              ]
             elif data['clouds'] >= 33 and data['clouds'] < 66:
-                responses = [f'A little bit from both sides.',
-                             f'Partly sunny, partly cloudy.',
-                             f'Not really sunny but not really cloudy either.'
+                responses = [f'{cloud_and_sun_emoji} A little bit from both sides in {location.capitalize()}.',
+                             f'{cloud_and_sun_emoji} Partly sunny, partly cloudy in {location.capitalize()}.',
+                             f'{cloud_and_sun_emoji} In {location.capitalize()} it is not really sunny but not really cloudy either.'
                              ]
             else:
-                responses = [f'No sun today.',
-                             f'Clouds, clouds and more clouds.',
-                             f'It is super cloudy.'
+                responses = [f'{cloud_emoji} No sun today in {location.capitalize()}.',
+                             f'{cloud_emoji} Clouds, clouds and more clouds in {location.capitalize()}.',
+                             f'{cloud_emoji} In {location.capitalize()} it is super cloudy.'
                              ]
 
             response = random.choice(responses)
@@ -329,45 +347,57 @@ async def on_message(message):
 # intent  general_weather
         elif intent == 2:
             location, time = entity_recognition(content)
+
+            sun_emoji = u'\U00002600'
+            cloud_emoji = u'\U00002601'
+            cloud_and_sun_emoji = u'\U000026C5'
+            thermo_emoji = u'\U0001F321'
+            little_rain_emoji = u'\U0001F326'
+            rain_emoji = u'\U0001F327'
+            wind_emoji = u'\U0001F32C'
+
+            if time == '':
+                time = 'today'
             data, alerts = get_general_weather(location, time)
             #response beginning
             #temperature
-            string = f"{time} in {location} the temperature ranges between {data['temp_min']}°C and {data['temp_max']}°C."
+            string = f"{time.capitalize()} the temperature in {location.capitalize()} ranges between {data['temp_min']}°C and {data['temp_max']}°C. {thermo_emoji}"
 
             if time == 'today':
-                string += f"Right now the temperature is {data['temp_now']}C°."
+                string += f"\nRight now the temperature is {data['temp_now']}C°. {thermo_emoji}"
 
             # cloudyness
-            string += f"\n{time} "
+            string += f"\n{time.capitalize()} "
             if data['clouds'] < 33:
-                string += f"it is sunny"
+                string += f"it is sunny. {sun_emoji}"
             elif data['clouds'] >= 33 and data['clouds'] < 66:
-                string += f"it is partly sunny and cloudy"
+                string += f"it is partly sunny and cloudy. {cloud_and_sun_emoji}"
             else:
-                string += f"it is very cloudy"
+                string += f"it is very cloudy. {cloud_emoji}"
 
             # rain
-            string += f"\n Also the probability for rain is {data['rain_prob']}%, so "
+            string += f"\nFurthermore the probability for rain is {data['rain_prob']}%, so "
             if data['rain_prob'] < 33:
-                string += f"it will not rain {time}"
+                string += f"it will not rain {time}. {sun_emoji}"
             elif data['rain_prob'] >= 33 and data['rain_prob'] < 66:
-                string += f"it might rain {time}"
+                string += f"it might rain {time}. {little_rain_emoji}"
             else:
-                string += f"it will rain {time}"
+                string += f"it will rain {time}. {rain_emoji}"
             # wind
-            string += f"\nThe wind is {data['wind']} km/h fast."
+            string += f"\nThe wind is {data['wind']} km/h fast. {wind_emoji}"
 
             response = string
 
 # intent greeting
         elif intent == 3:
-            responses = ['Hi there',
-                         'Hello',
-                         'Good day, sir.',
-                         'Hello, what can I do for you?',
-                         'Hey',
-                         'Salut',
-                         'Hello, my friend']
+            hello_emoji = u'\U0001F44B'
+            responses = [f'Hi there {hello_emoji}',
+                         f'Hello {hello_emoji}',
+                         f'Good day, sir. {hello_emoji}',
+                         f'Hello, what can I do for you? {hello_emoji}',
+                         f'Hey {hello_emoji}',
+                         f'Salute {hello_emoji}',
+                         f'Hello, my friend {hello_emoji}']
             response = random.choice(responses)
 
 # intent rain
@@ -375,23 +405,33 @@ async def on_message(message):
             location, time = entity_recognition(content)
             rain, rain_prob = get_rain(location, time)
 
+            if time == '':
+                time = 'today'
+
+            sun_emoji = u'\U00002600'
+            unbrella_emoji = u'\U00002614'
+            little_rain_emoji = u'\U0001F326'
+            rain_emoji = u'\U0001F327'
+            rain_coat_emoji = u'\U0001F9E5'
+            closed_umrella_emoji = u'\U0001F302'
+
             if rain == 'yes':
-                responses = [f'Yes, if you want to go outside {time}, you should take an umbrella.',
-                             f'Yes',
-                             f'Yes, the probability is {rain_prob}%'
+                responses = [f'The rain-probability for {time} in {location.capitalize()} is {rain_prob}%, you should take an umbrella. {unbrella_emoji}',
+                             f'The rain-probability for {time} in {location.capitalize()} is {rain_prob}%. {rain_emoji}',
+                             f'The rain-probability for {time} in {location.capitalize()} is {rain_prob}%, take a raincoat. {rain_coat_emoji}'
                              ]
 
-
             elif rain == 'no':
-                responses = [f'No, it is not very likely. The probability for {time} is only {rain_prob}%',
-                             f'No',
-                             f'No, the probability is {rain_prob}%'
+                responses = [f'The rain-probability for {time} in {location.capitalize()} is {rain_prob}%. You can safely leave your umbrella at home. {closed_umrella_emoji}',
+                             f'The rain-probability for {time} in {location.capitalize()} is {rain_prob}%. {sun_emoji}',
+                             f"The rain-probability for {time} in {location.capitalize()} is {rain_prob}%. You don't need a raincoat. {sun_emoji}",
+                             f"Keep calm, no rain {time}! {sun_emoji}"
                              ]
 
             elif rain == 'maybe':
-                responses = [f'Maybe yes, maybe no. Could be both.',
-                             f'Maybe',
-                             f'It might rain {time}, the probability is {rain_prob}%'
+                responses = [f'In {location.capitalize()}? Maybe yes, maybe no. Could be both. {little_rain_emoji}',
+                             f'In {location.capitalize()}? Maybe. Better keep your umbrella ready. {unbrella_emoji}',
+                             f'It might rain in {location.capitalize()} {time}, the probability is {rain_prob}%. {little_rain_emoji}'
                             ]
 
             response = random.choice(responses)
@@ -404,17 +444,27 @@ async def on_message(message):
             #print(sunset)
             sunrise = str(sunrise)[11:]
             sunset = str(sunset)[11:]
-            response = f'Sunrise: {sunrise} & Sunset: {sunset}'
+
+            sunrise_emoji = u'\U0001F305'
+            sunset_amoji = u'\U0001F307'
+
+            responses = [f'{sunrise_emoji} In {location.capitalize()} sunrise will be at {sunrise} UTC. \n{sunset_amoji} Sunset will be at {sunset} UTC.',
+                        f'{time.capitalize()} the sun in {location} will rise {sunrise_emoji} at {sunrise} and set {sunset_amoji} at {sunset} UTC.'
+                        f'{sunrise_emoji} The sun will rise in the morning at {sunrise} \n{sunset_amoji} and set in the evening at {sunset} UTC.',
+                        f"Why the hell are you asking, you nerd. You'll stay at home anyway. But if you really want to know ask again."]
+            response = random.choice(responses)
 
 # intent temperature
         elif intent == 6:
             location, time = entity_recognition(content)
             temp = get_temperature(location, time)
 
+            thermo_emoji = u'\U0001F321'
+
             if time == '':
-                responses = [f'Right now the temperature is {temp}C°',
-                             f'In {location} the temperature is {temp}C°',
-                             f'Outside it is {temp}C°']
+                responses = [f'{thermo_emoji} Right now the temperature is {temp}°C',
+                             f'{thermo_emoji} In {location} the temperature is {temp}°C',
+                             f'{thermo_emoji} Outside it is {temp}°C']
 
             elif time == 'today':
                 avg_temperature = int(((temp['morn'] - 273.15) + (temp['day'] - 273.15) + (temp['eve'] - 273.15) +
@@ -423,9 +473,9 @@ async def on_message(message):
                 temp_morn =  int(temp['morn'] - 273.15)
                 temp_eve =  int(temp['eve'] - 273.15)
                 temp_night =  int(temp['night'] - 273.15)
-                responses = [f"Today the average temperature is {avg_temperature}C°",
-                             f"In the morning it is {temp_morn}C° \nDuring the day it is {temp_day}C° \n In the evening it is {temp_eve}C° \n In the night it is {temp_night}C°",
-                             f"Today the temperature will rise up to {temp_day}°C"]
+                responses = [f"{thermo_emoji} Today the average temperature is {avg_temperature}°C",
+                             f"{thermo_emoji} In the morning it is {temp_morn}°C \n{thermo_emoji} During the day it is {temp_day}°C \n{thermo_emoji} In the evening it is {temp_eve}°C \n{thermo_emoji} In the night it is {temp_night}°C",
+                             f"{thermo_emoji} Today the temperature will rise up to {temp_day}°C"]
 
             elif time == 'tomorrow':
                 avg_temperature = int(((temp['morn'] - 273.15) + (temp['day'] - 273.15) + (temp['eve'] - 273.15) +
@@ -434,20 +484,30 @@ async def on_message(message):
                 temp_morn = int(temp['morn'] - 273.15)
                 temp_eve = int(temp['eve'] - 273.15)
                 temp_night = int(temp['night'] - 273.15)
-                responses = [f"Tomorrow the average temperature is {avg_temperature}C°",
-                             f"Tomorrow in the morning it will be {temp_morn}C° \nDuring the day it will be {temp_day}C° \n In the evening it will be {temp_eve}C° \n In the night it will be {temp_night}C°",
-                             f"Tomorrow the temperature will rise up to {temp_day}°C"]
+                responses = [f"{thermo_emoji} Tomorrow the average temperature is {avg_temperature}°C",
+                             f"{thermo_emoji} Tomorrow in the morning it will be {temp_morn}°C \n{thermo_emoji} During the day it will be {temp_day}°C \n{thermo_emoji} In the evening it will be {temp_eve}°C \n{thermo_emoji} In the night it will be {temp_night}°C",
+                             f"{thermo_emoji} Tomorrow the temperature will rise up to {temp_day}°C"]
 
             response = random.choice(responses)
 
 # intent unknown
         elif intent == 7:
-            responses = [f"{label_dict_inverse.get(intent)}, sorry can't help you!",
-                         f"Sorry, I don't know what you mean, please try again",
-                         f"BEEP BOOP, I am a robot and can tell you only about the weather",
-                         f"Help = No, Try again soon.",
-                         f"I don't really feel like answering you right now",
-                         f"Me and Google know everything, for this question you should consult Google."]
+
+            exploding_head_emoji = u'\U0001F92F'
+            skull_emoji = u'\U0001F480'
+            nerd_emoji = u'\U0001F913'
+            monokle_emoji = u'\U0001F9D0'
+            robot_emoji = u'\U0001F916'
+
+            responses = [f"{label_dict_inverse.get(intent)}, sorry can't help you! {monokle_emoji}",
+                         f"Sorry, I don't know what you mean, please try again {skull_emoji}",
+                         f"BEEP BOOP, I am a robot and can tell you only about the weather {robot_emoji}",
+                         f"Help = No, Try again soon. {robot_emoji}",
+                         f"I don't really feel like answering you right now {nerd_emoji}",
+                         f"Me and Google know everything, for this question you should consult Google. {nerd_emoji}",
+                         f"Sorry I am too underpaid for this shit! {exploding_head_emoji}",
+                         f"Shit I don't know. {exploding_head_emoji}",
+                         f"Bother Siri with this your stuff. {skull_emoji}"]
             response = random.choice(responses)
 
 # intent windy
@@ -455,20 +515,28 @@ async def on_message(message):
             location, time = entity_recognition(content)
             data, alerts = get_general_weather(location, time)
             wind = data['wind']
+
+            wind_emoji = u'\U0001F32C'
+            tornado_emoji = u'\U0001F32A'
+            no_wind_emoji = u'\U0001F32B'
+
+
+            if time == '':
+                time = 'right now'
             print(wind)
             if wind <= 3:
-                responses = [f'There is no wind. The wind speed is {wind}km/h',
-                             f'No wind.',
-                             f'There is no wind.']
+                responses = [f'{no_wind_emoji} There is no wind in {location.capitalize()}. The wind speed is {wind}km/h',
+                             f'{no_wind_emoji} No wind in {location.capitalize()} {time}.',
+                             f'{no_wind_emoji} There is no wind in {location.capitalize()}.']
             elif wind <= 10:
-                responses = [f'It is a soft breeze. The wind speed is {wind}km/h',
-                             f'There is a soft breeze.',
-                             f'A nice chilly breeze outside']
+                responses = [f'{wind_emoji} It is a soft breeze in {location.capitalize()}. The wind speed is {wind}km/h.',
+                             f'{wind_emoji} There is a soft breeze in {location.capitalize()} {time}.',
+                             f'{no_wind_emoji} A nice chilly breeze outside.']
 
             elif wind > 10:
-                responses = [f'It is windy. The wind speed is {wind}km/h.',
-                             f'It is a little windy',
-                             f'Maybe a hurrican.']
+                responses = [f'{tornado_emoji} It is windy. The wind speed is {wind}km/h.',
+                             f'{tornado_emoji} It is a very windy in {location.capitalize()} {time}.',
+                             f'{tornado_emoji} Maybe there is a hurrican in {location.capitalize()} {time}, because the wind speed is {wind}km/h.']
 
             response = random.choice(responses)
 
